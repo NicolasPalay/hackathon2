ac599b6229954c28840e8a39c38aaccb33614820
Estèphe Fath <estephefath@gmail.com>
2023-06-28 17:26:30 +0200

    create the CRUD for the entity Device

---
+++ src/Form/DeviceType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Device;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class DeviceType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('image')
+            ->add('stock')
+            //->add('memory')
+            //->add('storage')
+            //->add('sizeScreen')
+            //->add('camera')
+            //->add('state')
+            //->add('brand')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Device::class,
+        ]);
+    }
+}
